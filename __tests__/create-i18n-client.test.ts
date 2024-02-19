ba759ba2043f6c3376eaf71371040ba684e6092c
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-02-19 17:30:24 -0300

    Add @testing-library/react dependency and update import in create-i18n-client.test.js

--- __tests__/create-i18n-client.test.ts
+++ __tests__/create-i18n-client.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { createI18n } from "../src";
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, waitFor } from "@testing-library/react";
 
 describe("create-i18n-client", () => {
 	const { client } = createI18n({
@@ -8,22 +8,24 @@ describe("create-i18n-client", () => {
 	});
 
 	test("it translate function work", async () => {
-		const {result,waitForNextUpdate} = renderHook(() => client.useI18n());
-		await waitForNextUpdate();
-		expect(result.current("globals.usert_types.admin")).toBe("Admin");
+		const { result } = renderHook(() => client.useI18n());
+		await waitFor(() => {
+			expect(result.current("globals.usert_types.admin")).toBe("Admin");
+		});
 	});
 
 	test("it scoped translate function work", async () => {
-		const {result,waitForNextUpdate} = renderHook(() => client.useScopedI18n("globals"));
-		await waitForNextUpdate();
-		expect(result.current("usert_types.admin")).toBe("Admin");
+		const {result} = renderHook(() => client.useScopedI18n("globals"));
+		await waitFor(() => {
+			expect(result.current("usert_types.admin")).toBe("Admin");
+		});
 	});
 
-	test("it scoped translate function work", async () => {
-		const {result,waitForNextUpdate} = renderHook(() => client.useScopedI18n("globals"));
-		await waitForNextUpdate();
-		expect(result.current("usert_types.admin")).toBe("Admin");
+	test("it show path when not found", async () => {
+		const {result} = renderHook(() => client.useScopedI18n("globals"));
+		await waitFor(() => {
+			// @ts-expect-error - testing purpose
+			expect(result.current("notfound")).toBe("globals.notfound");
+		});
 	});
-
-
 });
