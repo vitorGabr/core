1ba83e2704825dd32046209664fe284a74595f21
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-03-01 15:12:05 -0300

    Add console logs for debugging

--- src/create/server/get-server-locale.ts
+++ src/create/server/get-server-locale.ts
@@ -4,10 +4,14 @@ export const getServerLocale = async <Locales extends ImportedLocales>(
 	locales: Locales,
 	options: LocaleServerOptions<typeof locales>,
 ) => {
+	console.log('aqui')
 	try {
 		const locale = (await options.storedLocale.get()) as keyof Locales;
+		console.log('locale', locale)
 		return (await locales[locale]).default;
 	} catch (error) {
+		console.log('error', error);
+		console.log('locale error', options.defaultLocale);
 		return (await locales[options.defaultLocale]).default;
 	}
 };
