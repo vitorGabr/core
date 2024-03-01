3fcd5e68161941558bc2d48243ab5cf17983d704
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-03-01 16:41:11 -0300

    Refactor getServerLocale function

--- src/create/server/get-server-locale.ts
+++ src/create/server/get-server-locale.ts
@@ -4,11 +4,6 @@ export const getServerLocale = async <Locales extends ImportedLocales>(
 	locales: Locales,
 	options: LocaleServerOptions<typeof locales>,
 ) => {
-	try {
-		const locale = await options.storedLocale.get();
-
-		return (await locales[locale as string]).default;
-	} catch (error) {
-		return (await locales[options.defaultLocale]).default;
-	}
+	const locale = options.storedLocale.get();
+	return (await locales[locale as string]).default;
 };
