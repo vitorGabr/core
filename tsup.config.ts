45aad297607ab78500f472f306d5a919a6e33877
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-03-01 13:31:50 -0300

    Update package.json and tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(({
 	entry: [
-		"src/create/index.ts",
+		"src/create/client/index.tsx",
+		"src/create/server/index.ts",
+		"src/create/default/index.ts",
 	],
 	dts: true,
 	clean: true,
