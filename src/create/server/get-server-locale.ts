4fb2866ecfdd7d034a6f95c17c29f1113ace1924
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-03-01 16:51:18 -0300

    Update getServerLocale to use default locale if stored locale is not available

--- src/create/server/get-server-locale.ts
+++ src/create/server/get-server-locale.ts
@@ -4,6 +4,6 @@ export const getServerLocale = async <Locales extends ImportedLocales>(
 	locales: Locales,
 	options: LocaleServerOptions<typeof locales>,
 ) => {
-	const locale = options.storedLocale.get();
+	const locale = options.storedLocale.get() ?? options.defaultLocale;
 	return (await locales[locale as string]).default;
 };
