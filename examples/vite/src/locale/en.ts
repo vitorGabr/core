7c107694b6f7500b45a1c59ccd0c108e4b6cbc37
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-03-26 08:48:37 -0300

    Add translations for home page

--- examples/vite/src/locale/en.ts
+++ examples/vite/src/locale/en.ts
@@ -2,4 +2,10 @@ export default {
     hello: 'Hello',
     world: 'World',
     welcome: 'Welcome',
+    pages: {
+        home: {
+            title: 'Home',
+            content: 'This is the home page'
+        }
+    }
 } as const
--- examples/vite/src/locale/pt-br.ts
+++ examples/vite/src/locale/pt-br.ts
@@ -2,4 +2,10 @@ export default {
     hello: 'Olá',
     world: 'Mundo',
     welcome: 'Bem-vindo',
+    pages: {
+        home: {
+            title: 'Página inicial',
+            content: 'Esta é a página inicial'
+        }
+    }
 } as const
